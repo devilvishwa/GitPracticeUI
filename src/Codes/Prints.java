482af2bed84347a6917b0e5676f1ce00b4a5753e
testervishwa <viishwanathnaik.test@gmail.com>
2023-03-18 20:35:39 +0530

    life is short.....

--- src/Codes/Prints.java
+++ src/Codes/Prints.java
@@ -4,5 +4,7 @@ public class Prints {
 	public static void main(String[] args) {
 		System.out.println("start........");
 		System.out.println("helloooo......");
+		System.out.println("byee......");
+		System.out.println("stopppppp......");
 	}
 }
